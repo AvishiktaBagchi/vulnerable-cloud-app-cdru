7beb990a8c03bed899d5248aa9176e1937357feb
Avishikta Bagchi <avi.bagchi04@gmail.com>
2026-03-24 13:18:03 -0500

    fixed sqlite issues

--- app/db.js
+++ app/db.js
@@ -1,5 +1,5 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./database.db");
+const fs = require("fs");
+const db = JSON.parse(fs.readFileSync("db.json"));
 
 // query
 function findUser(username, password, callback) {
